ebbc6b329e4872013a4332f50c60a15bd53ed5fd
Alain O'Dea <alain.odea@gmail.com>
2011-06-21 01:11:01 -0230

    Integrate with SyntaxHighlighter 3.0.83

---
+++ mod_syntaxhighlighter.erl
@@ -0,0 +1,28 @@
+%% @author Alain O'Dea <alain.odea@gmail.com>
+%% @copyright 2009 Marc Worrell
+%% @date 2011-06-19
+%% @doc The syntaxhighlighter module, implementing integration with Alex Gorbatchev's SyntaxHighlighter
+
+%%
+%% SyntaxHighlighter
+%% http://alexgorbatchev.com/SyntaxHighlighter
+%%
+%% SyntaxHighlighter is donationware. If you are using it, please donate.
+%% http://alexgorbatchev.com/SyntaxHighlighter/donate.html
+%%
+%% @version
+%% 3.0.83 (July 02 2010)
+%%
+%% @copyright
+%% Copyright (C) 2004-2010 Alex Gorbatchev.
+%%
+%% @license
+%% Dual licensed under the MIT and LGPL licenses.
+%%
+
+-module(mod_syntaxhighlighter).
+-author("Alain O'Dea <alain.odea@gmail.com>").
+
+-mod_title("SyntaxHighlighter").
+-mod_description("Alex Gorbatchev's client-side code highlighter").
+
---
+++ templates/_html_body.tpl
@@ -0,0 +1,12 @@
+{% lib "js/shCore.js"
+       "js/shBrushErlang.js"
+       "js/shBrushScala.js"
+       "js/shBrushJScript.js"
+       "js/shBrushCss.js"
+       "js/shBrushXml.js"
+       "js/shBrushPlain.js"
+       "js/shBrushJava.js"
+       "js/shBrushRuby.js"
+%}
+{% lib "css/shCoreDefault.css" %}
+<script>SyntaxHighlighter.all();</script>
